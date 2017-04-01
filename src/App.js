578464359c00117d4f964996986c901949f078fb
Saurabh Modh <saurabh.modh@hashedin.com>
2017-04-01 22:06:45 +0530

    first working redux application

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { createStore } from 'redux';
 import './App.css';
 
 class App extends Component {
@@ -10,5 +11,37 @@ class App extends Component {
     );
   }
 }
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
+const store = createStore(counter);
+console.log(store.getState());
+
+// store.dispatch({ type: "INCREMENT" });
+// console.log(store.getState());
+
+const render = () => {
+  document.body.innerHTML = store.getState();
+}
+
+// store.subscribe(() => {
+//   document.body.innerHTML = store.getState();
+// })
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+  store.dispatch({ type: "INCREMENT" });
+});
 
 export default App;
