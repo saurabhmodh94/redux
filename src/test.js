13f66825b48b982d89745099b95a99c853c5bb98
Saurabh Modh <saurabh.modh@hashedin.com>
2017-04-02 01:05:32 +0530

    avoiding-object-mutations-2

--- src/test.js
+++ src/test.js
@@ -1,76 +1,40 @@
 var deepFreeze = require('deep-freeze');
 var expect = require('expect')
 
-/*deepFreeze Example*/
-// deepFreeze(Buffer);
-// Buffer.x = 5;
-// console.log(Buffer.x === undefined);
+const toggleTodo = (todo) => {
+    // todo.completed = !todo.completed
+    // return todo;
 
-// Buffer.prototype.z = 3;
-// console.log(Buffer.prototype.z === undefined);
+    // return {
+    //     id:0,
+    //     text: 'Learn Redux',
+    //     completed: !todo.completed
+    // }; // normal solution
 
-const addCounter = (list) => {
-    // list.push(0);
-    // return list;
+    return Object.assign({},todo,{completed:!todo.completed})
 
-    // return list.concat(0); // normal solution
-    
-    return [...list, 0]
+    // return {
+    //     ...todo,
+    //     completed: !todo.completed
+    // } //not working
 }
 
-const testAddCounter = () => {
-    const listBefore = [];
-    const listAfter = [0];
-    deepFreeze(listBefore);
+const testToggleTodo = () => {
+    const todoBefore = {
+        id: 0,
+        text: 'Learn Redux',
+        completed: false
+    };
+    const todoAfter = {
+        id: 0,
+        text: 'Learn Redux',
+        completed: true
+    };
+    deepFreeze(todoBefore);
     expect(
-        addCounter(listBefore)
-    ).toEqual(listAfter);
+        toggleTodo(todoBefore)
+    ).toEqual(todoAfter);
 }
-testAddCounter();
-
-const removeCounter = (list, index) => {
-    // list.splice(index, 1);
-    // return list;
-
-    // return list
-    // .slice(0,index)
-    // .concat(list.slice(index+1)); // normal solution
-
-    return [...list.slice(0, index), ...list.slice(index + 1)]
-}
-
-const testRemoveCounter = () => {
-    const listBefore = [10, 20, 30];
-    const listAfter = [10, 30];
-    deepFreeze(listBefore);
-    expect(
-        removeCounter(listBefore, 1)
-    ).toEqual(listAfter);
-}
-testRemoveCounter();
-
-const incrementCounter = (list, index) => {
-    // list[index] = list[index]+1;
-    // return list;
-
-    // return list
-    // .slice(0,index)
-    // .concat(list[index]+1)
-    // .concat(list.slice(index+1)); // normal solution
-
-    return [...list.slice(0, index),
-    list[index] + 1,
-    ...list.slice(index + 1)]
-}
-
-const testIncrementCounter = () => {
-    const listBefore = [10, 20, 30];
-    const listAfter = [10, 21, 30];
-    deepFreeze(listBefore);
-    expect(
-        incrementCounter(listBefore, 1)
-    ).toEqual(listAfter);
-}
-testIncrementCounter();
+testToggleTodo();
 
 console.log("All Test Passed")
