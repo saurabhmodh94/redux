97aeb95ba33e5839d3a99f1d7163ab3999a3298d
Saurabh Modh <saurabh.modh@hashedin.com>
2017-04-02 19:37:57 +0530

    todo-list-reducer-toggling-a-todo

--- src/test.js
+++ src/test.js
@@ -12,6 +12,12 @@ const todos = (state = [], action) => {
                     completed: false
                 }
             ];
+        case 'TOGGLE_TODO':
+            return state.map(todo => {
+                if (todo.id != action.id)
+                    return todo;
+                return Object.assign({}, todo, { completed: !todo.completed })
+            })
         default:
             return state;
     }
@@ -36,5 +42,36 @@ const testAddTodo = () => {
     ).toEqual(stateAfter);
 }
 testAddTodo();
+
+const testToggleTodo = () => {
+    const stateBefore = [{
+        id: 0,
+        text: 'Redux Learn - 1',
+        completed: false
+    }, {
+        id: 1,
+        text: 'Redux Learn - 2',
+        completed: false
+    }];
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    }
+    const stateAfter = [{
+        id: 0,
+        text: 'Redux Learn - 1',
+        completed: false
+    }, {
+        id: 1,
+        text: 'Redux Learn - 2',
+        completed: true
+    }]
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
+}
+testToggleTodo();
 
 console.log("All Test Passed")
